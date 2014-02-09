006e24442944810418a5d9c556d073c3fcfe95ab
owlwise <jeffhoeft@charter.net>
2014-02-09 13:22:21 -0600

    Added test library

---
+++ creator/src/test/java/hootinanny/testAttribute.java
@@ -0,0 +1,14 @@
+package hootinanny;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAttribute {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
